323216da55973dd885e3f2f58e4b77e26915fc8b
Juho Enala <juho.enala@gmail.com>
2020-08-03 08:18:33 +0300

    Deploy scripts

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="switchboard-juhoenala",
+    name="Switchboard",
     version="0.0.1",
     author="Juho Enala",
     author_email="juho.enala@gmail.com",
