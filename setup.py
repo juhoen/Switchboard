7d97feec5752bd6ae18ae68f3b669030bbbfcd91
Juho Enala <juho.enala@gmail.com>
2020-08-03 08:24:21 +0300

    Changed pypi project name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Switchboard",
+    name="python-switchboard",
     version="0.0.1",
     author="Juho Enala",
     author_email="juho.enala@gmail.com",
